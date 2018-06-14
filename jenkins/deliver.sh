d3c1bbce55878b2b4238e7f437d85219f729dcbc
pjadda <jpavanaryan@gmail.com>
2018-06-14 17:50:36 -0400

    Updated jenkins file

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -24,6 +24,6 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-#!nohup java -jar target/${NAME}-${VERSION}.jar &
 ifconfig
-java -jar target/${NAME}-${VERSION}.jar
+nohup java -jar target/${NAME}-${VERSION}.jar &
+
