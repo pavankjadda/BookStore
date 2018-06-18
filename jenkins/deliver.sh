b6f2d173e58e3826702b28eb287ab5d424db0796
Pavan Jadda <17564080+pavankjadda@users.noreply.github.com>
2018-06-18 17:19:38 -0400

    Update deliver.sh

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo 'Install maven'
+apk add maven
+
+
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
