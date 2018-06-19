a6a22c4d6fb830bfd11da3848fa859458cef85ad
Pavan Jadda <17564080+pavankjadda@users.noreply.github.com>
2018-06-19 12:52:26 -0400

    Create deliver-local.sh

---
+++ jenkins/deliver-local.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+
+echo 'The following Maven command installs your Maven-built Java application'
+echo 'into the local Maven repository, which will ultimately be stored in'
+echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
+echo 'volume).'
+set -x
+mvn jar:jar install:install help:evaluate -Dexpression=project.name
+set +x
+
+echo 'The following complex command extracts the value of the <name/> element'
+echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+set -x
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME=${NAME,,}
+set +x
+
+echo 'The following complex command behaves similarly to the previous one but'
+echo 'extracts the value of the <version/> element within <project/> instead.'
+set -x
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+set +x
+
+echo 'The following command runs and outputs the execution of your Java'
+echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+set -x
+//ifconfig
+//nohup java -jar target/${NAME}-${VERSION}.jar &
+//java -jar target/${NAME}-${VERSION}.jar
+
+ 
+echo 'Building docker image of the Application'
+sudo docker build -f Dockerfile -t duppoc/bookstore:latest .
+
+echo 'Login into Dockerhub'
+sudo docker login -u duppoc -p Bcmc@2018
+
+echo 'Pushing to docker hub'
+sudo docker push duppoc/bookstore:latest
+
+echo 'Get docker image'
+sudo docker run  -d -p 8081:8081 duppoc/bookstore
