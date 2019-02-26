884f88b3ce9ea6ee66eb0889a38e04baad85d6dc
Pavan Jadda <jpavanaryan@gmail.com>
2019-02-26 18:12:54 -0500

    Bootstrap Version Upgraded

--- src/main/resources/static/js/save_book.js
+++ src/main/resources/static/js/save_book.js
@@ -31,6 +31,9 @@ app.config(function($routeProvider)
         .when("/", {
             templateUrl : "../index.html"
         })
+        .when("/view_book/:id", {
+        templateUrl : "../view_book.html"
+        })
         .when("/save_book", {
             templateUrl : "../save_book.html"
         })
--- src/main/resources/static/js/view_book.js
+++ src/main/resources/static/js/view_book.js
@@ -1,14 +1,19 @@
 var app = angular.module('view_book', ["ngRoute"]);
-app.controller('view_book_controller', function($scope,$http,$location)
+app.controller('view_book_controller', function($scope,$http,$routeParams)
 {
-    $scope.validate_and_save_book=function ()
+    $scope.get_book=function ()
     {
         $http({
             method : "GET",
-            url : "/books/"+id
+            url : "/books/"+$routeParams.id
         }).then(function mySuccess(response)
         {
-            $scope.books_data = response.data;
+            $scope.Id = response.data.id;
+            $scope.Title = response.data.title;
+            $scope.author = response.data.author;
+            $scope.number_of_pages = response.data.number_of_pages;
+            $scope.cost = response.data.cost;
+
         }, function myError(response)
         {
             $scope.myWelcome = response.statusText;
@@ -22,8 +27,9 @@ app.config(function($routeProvider)
         .when("/", {
             templateUrl : "../index.html"
         })
-        .when("/view_book", {
-            templateUrl : "../view_book.html"
+        .when("/view_book/:id", {
+            templateUrl : "../view_book.html",
+            controller: "view_book_controller"
         })
         .when("/save_book", {
             templateUrl : "../save_book.html"
