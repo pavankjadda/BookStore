9dc64aa39493357025ece5f68a676a5ef7862208
Pavan Jadda <jpavanaryan@gmail.com>
2019-03-01 18:13:37 -0500

    Updated Mappings

---
+++ src/main/resources/static/js/view_author.js
@@ -0,0 +1,42 @@
+var app = angular.module('view_book', ['ngRoute']);
+
+app.controller('view_book_controller', function($scope,$http, $route, $routeParams, $location)
+{
+    console.log('$location: '+$location);
+
+    $scope.get_book=function ()
+    {
+        $http({
+            method : "GET",
+            url : "/api/books/"+$location.$$path.replace('/','')
+        }).then(function mySuccess(response)
+        {
+            $scope.Id = response.data.id;
+            $scope.Title = response.data.title;
+            $scope.author = response.data.author;
+            $scope.number_of_pages = response.data.numberOfPages;
+            $scope.cost = response.data.cost;
+
+        }, function myError(response)
+        {
+            $scope.myWelcome = response.statusText;
+        });
+    };
+});
+
+app.config(function($routeProvider)
+{
+    $routeProvider
+        .when("/", {
+            templateUrl : "../index.html"
+        })
+        .when("/view_book/:id", {
+            templateUrl : "../view_book.html"
+        })
+        .when("/save_book", {
+            templateUrl : "../save_book.html"
+        })
+        .when("/books", {
+            templateUrl : "../books.html"
+        });
+});
