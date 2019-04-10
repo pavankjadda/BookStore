878c98229292fb30a6693f38ee4767ddf7f93a12
Pavan Jadda <jpavanaryan@gmail.com>
2019-04-10 12:38:00 -0400

    SpringBoot Upgraded

---
+++ src/main/java/com/books/aop/ControllerLogger.java
@@ -0,0 +1,44 @@
+package com.books.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * The type Controller logger.
+ */
+@Aspect
+@Component
+public class ControllerLogger
+{
+    private Logger logger = LoggerFactory.getLogger(ControllerLogger.class);
+
+    /**
+     * Case Controller pointcut.
+     *
+     * @param proceedingJoinPoint the proceeding join point
+     *
+     * @return the object
+     */
+    @Around("within(com.books.web.*)")
+    public Object logControllerRequests(ProceedingJoinPoint proceedingJoinPoint)
+    {
+        Object result;
+        try
+        {
+            result = proceedingJoinPoint.proceed();
+            logger.info("{} : Inside Controller {}.{}() method ", LocalDateTime.now(), proceedingJoinPoint.getSignature().getDeclaringTypeName(), proceedingJoinPoint.getSignature().getName());
+            return result;
+        }
+        catch (Throwable throwable)
+        {
+            logger.warn("{} : Exception occurred around method{}. Exception message: {}", LocalDateTime.now(), proceedingJoinPoint.getSignature(), throwable.getLocalizedMessage());
+        }
+        return null;
+    }
+}
